cae57761780a29cadae6f65b276e4ebc23e38d36
dor219 <dor219@lehigh.edu>
2023-12-19 17:02:06 -0500

    added route for playing files

--- src/main/java/com/bot/Spark.java
+++ src/main/java/com/bot/Spark.java
@@ -9,10 +9,18 @@ public class Spark {
         // Set up Spark
         port(4567); // Set the port for your Spark server
 
+        // For basic single line commands
         post("/bot_api/:param", (req, res) -> {
             String param = req.params(":param");
             bot.parseWebCommand(param);
-            return "Command received: " + param;
+            return "Attempting to perform command: " + param;
+        });
+
+        // Plays audio with a given filename
+        post("/bot_api/file/:name", (req, res) -> {
+            String name = req.params(":name");
+            bot.playFile(name);
+            return "Attempting to play file: " + name;
         });
 
         // Stop Spark gracefully on shutdown
