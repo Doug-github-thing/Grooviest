96cf899c627450580ac79ede794947f77fa13060
dor219 <dor219@lehigh.edu>
2023-12-19 12:09:37 -0500

    simultaneous Quart and Bot processes, terminate gracefully with interrupt

--- bot/bot.py
+++ bot/bot.py
@@ -2,28 +2,45 @@ import discord
 from discord.ext import commands
 import logging
 
-def run_bot(TOKEN):
-    # Initialize globals
-    bot = commands.Bot(command_prefix="-", intents=discord.Intents.default())
+class Bot(discord.Client):
+
     logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
+    def __init__(self):
+        self.bot = commands.Bot(command_prefix="-", intents=discord.Intents.default())
+        self._connection = self.bot._connection
 
-    # Runs on startup
-    @bot.event
-    async def on_ready():
-        await connect()
+        # Runs on startup
+        @self.bot.event
+        async def on_ready():
+            logging.info(f"Logged in as {self.bot.user.name}")
+            logging.info(f"Connection: {self.bot._connection}")
+            logging.info(f"Guilds: {self.bot._connection.guilds}")
 
+        @self.bot.command(name='ping')
+        async def ping(ctx):
+            await ctx.send('PONG')
 
     # Connects the bot to the default voice channel
-    async def connect():
-        logging.info("~~~~~~~~~~~~~Connecting~~~~~~~~~~~~~")
+    async def join(self):
+        logging.info("~~~~~~~~~~~~~Connecting To Voice~~~~~~~~~~~~~")
+        logging.info(f"My Connection object: {self.bot._connection}")
+        logging.info(f"My connected guilds: {self.bot._connection.guilds}")
 
         # Search for the correct channel
-        for guild in bot.guilds:
+        for guild in self.bot.guilds:
             if (guild.name == '\U0001d610\'\U0001d62e \U0001d62a\U0001d62f'):
-                for channel in guild.channels:
+
+                logging.info("Found I'm In")
+
+                for channel in  guild.channels:
                     if channel.name == "General Cannoli":
-                        logging.info(f"Connected to voice channel {channel.name}")
+                        logging.info(f"Connecting to voice channel {channel.name}")
                         await channel.connect()
 
-    bot.run(TOKEN)
+        # await self.bot.close()
+        
+
+    # Run the bot
+    def begin(self, TOKEN):
+        self.bot.run(TOKEN)
--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,60 @@
-# Core bot function
-import bot
-
-# Globals
+# For starting the bot
 import os
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
+from bot import Bot
+
+global bot
+my_bot = Bot()
+
+# For starting the server
+from quart import Quart, request
 import logging
-
-
-# Initialize globals
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
 
-if(__name__=="__main__"):
-    bot.run_bot(TOKEN)
+# Quart setup
+app = Quart(__name__)
+
+@app.route('/')
+async def index():
+    await my_bot.join()
+    print("Receieved HTTP")
+    return 'Hello, this is the web server!'
+
+@app.route('/webhook', methods=['POST'])
+async def webhook():
+    data = await request.get_json()
+    print(f'Received webhook data: {data}')
+    # Perform bot actions based on the received data
+    # You can customize this part based on your requirements
+    return 'Webhook received successfully'
+
+
+def start_bot():
+    load_dotenv()
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    my_bot.begin(TOKEN)
+
+def start_web():
+    app.run(port=5000)
+
+if __name__ == '__main__':
+    # Run web server in a separate Process
+    from multiprocessing import Process # for multitrack drifting
+
+    # Run Quart and Discord bot in separate processes
+    quart_process = Process(target=start_web)
+    bot_process = Process(target=start_bot)
+
+    try:
+        quart_process.start()
+        bot_process.start()
+
+        quart_process.join()
+        bot_process.join()
+
+    except KeyboardInterrupt:
+        # Handle KeyboardInterrupt (Ctrl+C) and terminate the processes
+        print("Terminating processes")
+        bot_process.terminate()
+        quart_process.terminate()
