7237dae7707b64bfc2150977a442a4a3fc687e42
dor219 <dor219@lehigh.edu>
2023-12-19 17:01:43 -0500

    removed placeholder Hello World

--- src/main/java/com/bot/Main.java
+++ src/main/java/com/bot/Main.java
@@ -4,7 +4,6 @@ import com.bot.Bot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         String token = System.getenv("DISCORD_TOKEN");
         Bot bot = new Bot(token);
