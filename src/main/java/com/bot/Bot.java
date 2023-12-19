9b21f2a8bbcd54e4dbeacba729872ab63853a769
dor219 <dor219@lehigh.edu>
2023-12-19 16:50:31 -0500

    bot can now leave and rejoin the channel from web commands

--- src/main/java/com/bot/Bot.java
+++ src/main/java/com/bot/Bot.java
@@ -3,25 +3,90 @@ package com.bot;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
+import net.dv8tion.jda.api.managers.AudioManager;
 
-import javax.security.auth.login.LoginException;
+import java.util.List;
 
 public class Bot extends ListenerAdapter {
 
+    Guild myGuild;
+    AudioManager audioManager;
+    AudioChannelUnion audioChannel;
+
     public Bot(String TOKEN) {
         JDABuilder builder = JDABuilder.createDefault(TOKEN);
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
         builder.addEventListeners(this);
         builder.build();
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.getAuthor().isBot() && event.getMessage().getContentRaw().equalsIgnoreCase("!egg")) {
-            event.getChannel().sendMessage("Hello, " + event.getAuthor().getAsMention() + "!").queue();
+
+        if (event.getAuthor().isBot())
+            return;
+        if (!event.getMessage().getContentRaw().startsWith("-"))
+            return;
+        if (!event.isFromGuild())
+            return;
+
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("-egg")) {
+            event.getChannel().sendMessage("emngmgg").queue();
+        }
+
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("-join")) {
+            myGuild = event.getGuild();
+
+            // Let 'em know
+            String msg = "Now listening in: " + event.getGuild() + "!";
+            event.getChannel().sendMessage(msg).queue();
+
+            // Connect to the voice channel
+            audioManager = myGuild.getAudioManager();
+            audioChannel = event.getMember().getVoiceState().getChannel();
+
+            audioManager.openAudioConnection(audioChannel);
         }
     }
 
-    public void touch(String str) {
-        System.out.println("I am the bot and I am getting from the server: " + str);
+    public void parseWebCommand(String str) {
+        if (myGuild == null)
+            return;
+
+        System.out.println("I am the bot. I'm in the server: " + myGuild);
+
+        switch (str) {
+            case "join":
+                join();
+                break;
+            case "leave":
+                leave();
+                break;
+            case "play":
+                play();
+                break;
+            default:
+                break;
+        }
     }
+
+    public void join() {
+        System.out.println("Attempting to join the channel");
+        audioManager.openAudioConnection(audioChannel);
+        audioManager.openAudioConnection(audioChannel);
+    }
+
+    public void leave() {
+        System.out.println("Attempting to leave the channel");
+        audioManager.closeAudioConnection();
+    }
+
+    public void play() {
+        System.out.println("I was told to play audio!");
+    }
+
 }
--- src/main/java/com/bot/Spark.java
+++ src/main/java/com/bot/Spark.java
@@ -9,12 +9,10 @@ public class Spark {
         // Set up Spark
         port(4567); // Set the port for your Spark server
 
-        get("/bot_api/:param", (req, res) -> {
+        post("/bot_api/:param", (req, res) -> {
             String param = req.params(":param");
-            bot.touch(param);
-            res.status(200);
-            res.body("Heggo!");
-            return "You said, " + param + "!";
+            bot.parseWebCommand(param);
+            return "Command received: " + param;
         });
 
         // Stop Spark gracefully on shutdown
