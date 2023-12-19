257715db2a8025b7538d7a34a7d776102ad94f30
dor219 <dor219@lehigh.edu>
2023-12-19 15:45:56 -0500

    formatting

--- src/main/java/com/bot/Main.java
+++ src/main/java/com/bot/Main.java
@@ -2,11 +2,9 @@ package com.bot;
 
 import com.bot.Bot;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
 
         String token = System.getenv("DISCORD_TOKEN");
         Bot bot = new Bot(token);
