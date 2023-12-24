cc988c27fc51342fa6080614aa97b898dcd09eff
dor219 <dor219@lehigh.edu>
2023-12-23 20:58:42 -0500

    updated bot backend url

--- grooviest-web/src/api/API.js
+++ grooviest-web/src/api/API.js
@@ -8,9 +8,9 @@ export default class API {
      * @param command name of the command, ie "leave" or "join"
      */
     static sendCommand = (command) => {
-        const resource = `https://${process.env.REACT_APP_BOT_ADDRESS}:25566/bot_api/${command}`;
-        console.log(resource);
-        
+        const resource = `${process.env.REACT_APP_BOT_ADDRESS}/bot_api/${command}`;
+        console.log(`Sending '${command}' to the bot`);
+
         fetch(resource, {
             method: "POST"
         }).then(res => { 
