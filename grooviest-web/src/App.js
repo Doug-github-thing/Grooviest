4e97ad6629efc8269a074b14db71a89fbdb0812a
dor219 <dor219@lehigh.edu>
2023-12-25 12:02:51 -0500

    moved around to make room for the database when it's ready

--- grooviest-web/src/App.js
+++ grooviest-web/src/App.js
@@ -3,13 +3,33 @@ import CommandButton from "./components/CommandButton";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h>Groovy Web app</h>
-        <CommandButton text="Join" command="join" />
-        <CommandButton text="Leave" command="leave" />
-        <CommandButton text="a" command="file/a.mp3" />
+    <div className="app">
+
+      <header className="app-header">
+        <h1>Groovy Web app</h1>
+        <div className="control-buttons">
+          <CommandButton text="Join" command="join" />
+          <CommandButton text="Leave" command="leave" />
+        </div>
       </header>
+
+      <body>
+
+        <div className="column left">
+          <h2>Soundbar</h2>
+          <CommandButton text="a" command="file/a.mp3" />  
+        </div>
+
+        <div className="column right">
+          <h2>Database:</h2>
+          <div>Once connected, the contents of the database will show here</div>
+          <div>and also here</div>
+        </div>
+
+      {/* End Column Scaffold */}
+      </body>
+    
+    {/* End app div */}
     </div>
   );
 }
