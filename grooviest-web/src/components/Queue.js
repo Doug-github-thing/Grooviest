54688bb50b0d99ac3896b9d89fd458e92f0529a7
dor219 <dor219@lehigh.edu>
2023-12-26 16:30:19 -0500

    queue updates correctly when database is updated

--- grooviest-web/src/components/Queue.js
+++ grooviest-web/src/components/Queue.js
@@ -9,31 +9,30 @@ import "./Queue.css";
 
 const Queue = ({ className }) => {
 
-    const [songs, setSongs] = useState([]);
-
     const db = getDatabase();
+
+    const [songs, setSongs] = useState([]);
 
     useEffect(() => {
         const query = ref(db, "songs");
         return onValue(query, (snapshot) => {
-          const data = snapshot.val();
-          console.log(snapshot.toJSON());
-    
-          if (snapshot.exists()) {
-            Object.values(data).map((song) => {
-              setSongs((songs) => [...songs, song]);
-            });
-          }
+            const data = snapshot.val();
+            console.log(snapshot.toJSON());
+  
+            if (snapshot.exists()) {
+                const newSongs = Object.values(data);
+                setSongs(newSongs);
+            }
         });
-      }, []);
+    }, []);
 
     return (
         <div className={className}>
             <h2>Queue</h2>
             <div>
                 {songs.map((song, index) => (
-                    <div className="song-wrapper">
-                      <div key={index}> {index} Name: {song.name} URL: {song.url} </div>
+                    <div className="song-wrapper" key={index}>
+                      <div> {index + 1} Name: {song.name} URL: {song.url} </div>
                       <CommandButton text="-" command={`remove/${index}`}/>
                     </div>
                 ))}
