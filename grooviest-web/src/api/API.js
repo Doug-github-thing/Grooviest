a01ca974119afcb98bea7d5d9b74bdca79e0408a
dor219 <dor219@lehigh.edu>
2023-12-27 23:38:53 -0500

    adds youtube video search and add

--- grooviest-web/src/api/API.js
+++ grooviest-web/src/api/API.js
@@ -4,6 +4,7 @@
 export default class API {
 
     static bot_address = process.env.REACT_APP_BOT_ADDRESS;
+    static youtube_api_key = process.env.REACT_APP_YOUTUBE_API_KEY;
     // static bot_address = "http://127.0.0.1:25566";
 
     /**
@@ -18,6 +19,20 @@ export default class API {
             method: "POST"
         }).then(res => { 
             console.log(`Response from bot: ${res.status}, ${res.statusText}`);
+        });
+    }
+
+    static youtubeSearch = async (terms) => {
+        const base = "https://www.googleapis.com/youtube/v3/search?part=snippet";
+        const search = `&q=${encodeURIComponent(terms)}`;
+        const key= `&key=${this.youtube_api_key}`;
+
+        const resource = base + search + key;
+
+        return await fetch(resource, {
+            method: "GET"
+        }).then(res => {
+            return res.json();
         });
     }
 }
--- grooviest-web/src/components/TextForm.js
+++ grooviest-web/src/components/TextForm.js
@@ -2,27 +2,45 @@ import { useState } from "react";
 
 // To send commands to the bot API.
 import API from "../api/API";
+import CommandButton from "./CommandButton";
 
 const TextForm = ({ label }) => {
 
-    const [search, setSearch] = useState("");
+    const [searchTerms, setSearchTerms] = useState("");
+    const [searchResults, setSearchResults] = useState(null);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setSearch(event.target.value);
-        API.sendCommand(`add/${search}`);
-        setSearch("");
+    // When the search button is pressed
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Do not refresh the page
+
+        const searchResults = await API.youtubeSearch(searchTerms);
+        setSearchResults(searchResults.items); 
+
+        setSearchTerms(""); // Clear search terms once searched
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                {label}
-                <input type="text" name="youtube" value={search}
-                    onChange={(e) => setSearch(e.target.value)}/>
-            </label>
-            <input type="submit" value="+" />
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    {label}
+                    <input type="text" name="youtube" value={searchTerms}
+                        onChange={(e) => setSearchTerms(e.target.value)} />
+                </label>
+                <input type="submit" value="Search" />
+            </form>
+
+            {searchResults == null ? <></> : 
+            searchResults.map((video, index) => (
+                <div className="video-wrapper" key={index}>
+                    <div className="id">
+                        <div className="name">Name: {video.snippet.title}</div>
+                        <div className="url">channel: {video.snippet.channelTitle}</div>
+                    </div>
+                    <CommandButton className="button" text="+" command={`add/${video.id.videoId}`}/>
+                </div>
+            ))}
+        </div>
     );
 };
 
