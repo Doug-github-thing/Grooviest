a173b0eef385e9baebff6cac87fcb8e01889dc5b
dor219 <dor219@lehigh.edu>
2023-12-31 11:58:11 -0500

    search results parsed and structured cleanly as array of simpler objects, separated result card component

--- grooviest-web/src/components/SearchBar.js
+++ grooviest-web/src/components/SearchBar.js
@@ -3,21 +3,52 @@ import { useState } from "react";
 // To send commands to the bot API.
 import API from "../api/API";
 import CommandButton from "./CommandButton";
+import SearchResultCard from "./SearchResultCard";
 
-const TextForm = ({ label }) => {
+const SearchBar = ({ label }) => {
 
+    // The String search terms used.
     const [searchTerms, setSearchTerms] = useState("");
+    // Search Results expected to be the form of an Array of search result objects,
+    // which contain video metadata. Null if there is an error, empty Array if 0 results.
     const [searchResults, setSearchResults] = useState(null);
 
-    // When the search button is pressed
-    const handleSubmit = async (event) => {
+    /**
+     * When the search button is pressed, query YoutubeAPI, and handle parsing their
+     * response into an array of Result objects, or null in the event of an error.
+     */
+    const handleVideoSearch = async (event) => {
         event.preventDefault(); // Do not refresh the page
 
-        const searchResults = await API.youtubeSearch(searchTerms);
-        console.log("Search resutls: ", searchResults.items);
-        setSearchResults(searchResults.items); 
+        const rawSearchResults = await API.youtubeSearch(searchTerms);
+        // console.log("Search resutls: ", rawSearchResults.items);
 
-        setSearchTerms(""); // Clear search terms once searched
+        // Check if response has expected shape. If not, return
+        if (!rawSearchResults.hasOwnProperty("pageInfo") 
+            || !rawSearchResults.pageInfo.hasOwnProperty("resultsPerPage")) {
+            setSearchResults(null);
+            return;
+        }
+        
+        // Find how many results there are
+        const resultsCount = rawSearchResults.pageInfo["resultsPerPage"];
+        let resultsArray = new Array(resultsCount);
+
+        // Iterate through them, converting each result into a easy to read object
+        for (let i = 0; i < resultsCount; i++) {
+            resultsArray[i] = {
+                id: rawSearchResults.items[i].id.videoId,
+                name: rawSearchResults.items[i].snippet.title,
+                channel: rawSearchResults.items[i].snippet.channelTitle,
+                thumbnail: rawSearchResults.items[i].snippet.thumbnails.default.url,
+                duration: 666
+            }
+        }
+        console.log("Parsed these search results: ", resultsArray);
+        
+        // Set Results Array, and clear search terms once searched is finished
+        setSearchResults(resultsArray);
+        setSearchTerms("");
     }
 
     // Called when "add song" button is pressed.
@@ -30,7 +61,7 @@ const TextForm = ({ label }) => {
     return (
         <div>
             {/* Search bar */}
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleVideoSearch}>
                 <label>
                     {label}
                     <input type="text" name="youtube" value={searchTerms}
@@ -41,18 +72,19 @@ const TextForm = ({ label }) => {
 
             {/* Search Results */}
             {searchResults == null ? <></> :
-            searchResults.map((video, index) => (
-                <div className="video-wrapper" key={index}>
-                    <img className="thumbnail" src={video.snippet.thumbnails.default.url} />
-                    <div className="id">
-                        <div className="name">Name: {video.snippet.title}</div>
-                        <div className="url">channel: {video.snippet.channelTitle}</div>
-                    </div>
-                    <button onClick={() => { addSongCallback(video.id.videoId) }}>+</button>
-                </div>
+            searchResults.map((result, index) => (
+                <SearchResultCard result={result} removeSearchResultsCallback={setSearchResults}/>
+                // <div className="video-wrapper" key={index}>
+                //     <img className="thumbnail" src={video.snippet.thumbnails.default.url} />
+                //     <div className="id">
+                //         <div className="name">Name: {video.snippet.title}</div>
+                //         <div className="url">channel: {video.snippet.channelTitle}</div>
+                //     </div>
+                //     <button onClick={() => { addSongCallback(video.id.videoId) }}>+</button>
+                // </div>
             ))}
         </div>
     );
 };
 
-export default TextForm;
+export default SearchBar;
---
+++ grooviest-web/src/components/SearchResultCard.js
@@ -0,0 +1,37 @@
+import API from "../api/API";
+
+/**
+ * Formats a Youtube search result, passed as an object with the form:
+ * {
+ *      "id": youtubeID,
+ *      "title": title,
+ *      "channel": channel name,
+ *      "thumbnail": link to thumbnail,
+ *      "duration": duration
+ * } 
+ * @param {YoutubeSearchResultItem} result The search result object formatted as specified above
+ * @param {function} setSearchResults Function callback for when an "add" button is clicked, to clear
+ *                                    the search results from the screen
+ */
+const SearchResultCard = ({result, removeSearchResultsCallback}) => {
+
+    // Called when "add song" button is pressed.
+    // Removes search results from the screen.
+    const addSongCallback = (command) => {
+        API.sendCommand("add/" + command);
+        removeSearchResultsCallback(null);
+    }
+
+    return (
+        <div className="search-result-card">
+            
+            <img className="thumbnail" src={result.thumbnail} />
+            <div className="name">Name: {result.title}</div>
+            <div className="url">channel: {result.channel}</div>
+            <button onClick={() => { addSongCallback(result.id) }}>+</button>
+
+        </div>
+    )
+}
+
+export default SearchResultCard;
