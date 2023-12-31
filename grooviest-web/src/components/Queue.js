1c97ea7bd8d01d50f56cd988ea3388f6fe4edc40
dor219 <dor219@lehigh.edu>
2023-12-31 13:00:29 -0500

    queue now properly displays as emptied

--- grooviest-web/src/components/Queue.js
+++ grooviest-web/src/components/Queue.js
@@ -9,35 +9,38 @@ import "./Queue.css";
 
 const Queue = ({ className }) => {
 
+
+    // For tracking which songs are currently in the queue.
     const [songs, setSongs] = useState([]);
 
+
+    // Called each time there is an update to the queue in the database.
+    // If it is now empty, set the state to an empty queue. Else, extract those values.
     useEffect(() => {
         const db = getDatabase();
         const query = ref(db, "songs");
         return onValue(query, (snapshot) => {
             const data = snapshot.val();
-  
-            if (snapshot.exists()) {
-                const newSongs = Object.values(data);
-                setSongs(newSongs);
-            }
+            snapshot.exists() ? setSongs(Object.values(data)) : setSongs();    
         });
     }, []);
+
 
     return (
         <div className={className}>
             <h2>Queue</h2>
             <div>
-                {songs.map((song, index) => (
+                {songs == null ? <></> :
+                    songs.map((song, index) => (
 
-                    <div className="song-wrapper" key={index}>
-                        <div className="id">
-                            <div className="name">Name: {song.name}</div>
-                            <div className="url">URL: {song.url}</div>
+                        <div className="song-wrapper" key={index}>
+                            <div className="id">
+                                <div className="name">Name: {song.name}</div>
+                                <div className="url">URL: {song.url}</div>
+                            </div>
+                            <CommandButton className="button" text="-" command={`remove/${song.position}`}/>
                         </div>
-                        <CommandButton className="button" text="-" command={`remove/${song.position}`}/>
-                    </div>
-                ))}
+                    ))}
             </div>
         </div>
     );
