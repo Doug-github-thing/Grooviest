fcd653cc5974fe55cf24258053ed487b4a92a9cf
dor219 <dor219@lehigh.edu>
2024-01-02 17:25:06 -0500

    seeking via the progress bar works now

--- grooviest-web/src/components/ProgressBar.js
+++ grooviest-web/src/components/ProgressBar.js
@@ -3,19 +3,26 @@ import { ref, getDatabase, onValue } from "firebase/database";
 
 // For displaying ms timestamps as human readable
 import PlayerUtils from "../utils/PlayerUtils";
+// For sending Seeking requests on bar click
+import API from "../api/API";
 // For displaying linear progress bar
 import "./ProgressBar.css";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 /**
  * @returns A linear progress bar for visualizing how for you are into the current song 
  */
 const MyProgressBar = () => {
-    // Tracks seeking progress as Numbers
+    // Keeps track of song progress as Numbers
     const [duration, setDuration] = useState(0);
     const [elapsed, setElapsed] = useState(0);
+    // For detecting what position on the bar the mouse is hovering
+    const [hoveredValue, setHoveredValue] = useState(null);
+    // For tracking when the mouse is hovering the bar
+    const [isHovered, setIsHovered] = useState(false);
 
     
-    // For track seeking
+    // For visualizing song progress
     useEffect(() => {
         const myDatabase = getDatabase();
         const query = ref(myDatabase, "now_playing/duration");
@@ -27,7 +34,7 @@ const MyProgressBar = () => {
         });
     }, []);
 
-    // For track seeking
+    // For visualizing song progress
     useEffect(() => {
         const myDatabase = getDatabase();
         const query = ref(myDatabase, "now_playing/elapsed");
@@ -40,9 +47,30 @@ const MyProgressBar = () => {
     }, []);
 
 
+    // Keeping track of where the mouse is currently seeking
+    const handleMouseOver = (event) => {
+        setIsHovered(true);
+
+        // If there's no song selected, do nothing
+        if (duration === 0) {
+            setHoveredValue(0);
+            setIsHovered(false);
+            return;
+        }
+        
+        // Calculate the value based on the mouse position
+        const rect = event.currentTarget.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const totalWidth = rect.width;
+        const ms = Math.ceil(duration * (x / totalWidth));
+        
+        // Update the hovered values
+        setHoveredValue(ms);
+    };
+
+
     return (
         <div className="progress-bar">
-
             {duration === 0 ? 
                 // If there's no song playing, just show an empty bar
                 <div className="progress-bar-outside" />
@@ -51,17 +79,25 @@ const MyProgressBar = () => {
                 <>
                     <div className="left">{PlayerUtils.formatTime(elapsed)}</div>
 
-                    <div className="progress-bar-outside">
+                    <div className="progress-bar-outside"
+                        style={{height: isHovered ? "2.5vmin" : "0.5vmin"}}
+                        onMouseMove={handleMouseOver} 
+                        onMouseOut={() => {setIsHovered(false)}}
+                        onClick={() => {API.sendCommand(`seek/${hoveredValue}`)}}
+                    >
                         {duration === 0 ? <></> :
                             <div className="progress-bar-inside" 
-                                style={{width:`${100 * (elapsed / duration)}%`}} />
+                                style={{width:`${100 * (elapsed / duration)}%`,
+                                    height: isHovered ? "2.5vmin" : "0.5vmin"}}
+                                onMouseOut={() => {setIsHovered(false)}}
+                                onClick={() => {API.sendCommand(`seek/${hoveredValue}`)}}
+                            />
                         }
                     </div>
 
-                    <div className="right">{PlayerUtils.formatTime(duration)}</div>   
+                    <div className="right">{PlayerUtils.formatTime(duration)}</div>
                 </>
             }
-
         </div>
     );
     
